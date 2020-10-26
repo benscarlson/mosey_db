d91672f00b8498b87cae87e6e0fb1666d37c786d
Ben Carlson <ben.s.carlson@gmail.com>
2020-10-26 10:55:41 -0400

    initialize repo

---
+++ funs/themes.r
@@ -0,0 +1,20 @@
+theme_eda <- theme_classic(base_family = 'Helvetica') +
+  theme(
+    plot.title = element_text(size=20),
+    plot.tag = element_text(size=18),
+    axis.text=element_text(size=10),
+    axis.title=element_text(size=14),
+    legend.text = element_text(size=10),
+    legend.title = element_text(size=12),
+    strip.text = element_text(size=12)
+  )
+
+theme_ms <- theme_classic(base_family='Helvetica') +
+  theme(
+    plot.title = element_text(size=20),
+    plot.tag = element_text(size=18),
+    axis.text=element_text(size=12),
+    axis.title=element_text(size=14),
+    legend.text = element_text(size=12),
+    legend.title = element_text(size=14)
+  )
---
+++ startup.r
@@ -0,0 +1,22 @@
+
+message('Running startup script...')
+
+options(stringsAsFactors=FALSE)
+options(dplyr.summarise.inform = FALSE) #https://www.tidyverse.org/blog/2020/05/dplyr-1-0-0-last-minute-additions/
+options(tidyverse.quiet = TRUE) #https://rstats-tips.net/2020/07/31/get-rid-of-info-of-dplyr-when-grouping-summarise-regrouping-output-by-species-override-with-groups-argument/
+
+suppressWarnings(
+suppressPackageStartupMessages({
+  library(assertthat)
+  library(conflicted)
+  library(glue)
+  library(here)
+  library(tidyverse)
+  library(uuid)
+}))
+
+
+
+# Several common packages have conflicts with dplyr filter() and select()
+conflict_prefer('filter','dplyr',quiet=TRUE)
+conflict_prefer('select','dplyr',quiet=TRUE)
